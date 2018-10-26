649c0e78987cb0ec55ec808a8d96fa1bf8f31da1
Kevin Liu <800018681@fjuhsd.org>
2018-10-26 11:03:38 -0700

    School

--- Lab14/Bumper.java
+++ Lab14/Bumper.java
@@ -1,65 +1,102 @@
-// Name:                Date:
+import java.awt.Color;
+import java.awt.Graphics;
 
-import java.awt.*;
+
 
 public class Bumper
 {
-    int x, y, xWidth, yWidth;
-    Color c;
-    public Bumper()         //default constru   ctor
+    private int myX;
+    private int myY;
+    private int myXWidth;
+    private int myYWidth;
+    private Color myColor;
+
+    public Bumper()
     {
-        x = 50;
-        y = 50;
-        xWidth = 10;
-        yWidth = 10;
-        c = Color.BLACK;
+        myX = 200;
+        myY = 200;
+        myXWidth = 25;
+        myYWidth = 50;
+        myColor = Color.BLUE;
     }
 
-    public Bumper(int x, int y, int xWidth, int yWidth, Color c)
-    {
-
+    public Bumper(int x, int y, int xWidth, int yWidth, Color c) {
+        myX = x;
+        myY = y;
+        myXWidth = xWidth;
+        myYWidth = yWidth;
+        myColor = c;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y
-     */
-    public int sampleMethod(int y)
+    public int getX()
     {
-        // put your code here
-        return y;
+        return myX;
     }
 
-    // modifier methods  (one for each field)
+    public int getY() {
+        return myY;
+    }
 
-    // instance methods
-    // chooses a random (x,y) location.  Bumper stays entirely in the window.
+    public int getXWidth() {
+        return myXWidth;
+    }
+
+    public int getYWidth() {
+        return myYWidth;
+    }
+
+    public Color getColor() {
+        return myColor;
+    }
+
+    public void setX(int x)
+    {
+        myX = x;
+    }
+
+    public void setY(int y) {
+        myY = y;
+    }
+
+    public void setXWidth(int xWidth) {
+        myXWidth = xWidth;
+    }
+
+    public void setYWidth(int yWidth) {
+        myYWidth = yWidth;
+    }
+
+    public void setColor(Color c) {
+        myColor = c;
+    }
+
+
     public void jump(int rightEdge, int bottomEdge)
     {
-        // moves location to random (x, y) within the edges
-
+        myX = ((int)(Math.random() * (rightEdge - myXWidth)));
+        myY = ((int)(Math.random() * (bottomEdge - myYWidth)));
     }
 
-    // draws a rectangular bumper on the buffer
-    public void draw(Graphics myBuffer) 
+    public void draw(Graphics myBuffer)
     {
         myBuffer.setColor(getColor());
         myBuffer.fillRect(getX(), getY(), getXWidth(), getYWidth());
-    }   
-    // returns true if any part of the polkadot is inside the bumper
+    }
+
     public boolean inBumper(Polkadot dot)
     {
-        for(int x = getX(); x <= getX() + getXWidth(); x++)   //starts at upper left corner(x,y)
-            for(int y = getY(); y <= getY() + getYWidth(); y++)
-                if(distance(x, y, dot.getX(), dot.getY()) <= dot.getRadius() ) //checks every point on the bumper
-                    return true;            
+        for (int x = getX(); x <= getX() + getXWidth(); x++){
+            for (int y = getY(); y <= getY() + getYWidth(); y++){
+                if (distance(x, y, dot.getX(), dot.getY()) <= dot.getRadius()){
+                    return true;
+                }
+            }
+        }
         return false;
-    }  
-    // returns distance between (x1, y1) and (x2, y2)
+    }
+
     private double distance(double x1, double y1, double x2, double y2)
     {
-        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
-    }   
+        return Math.sqrt(Math.pow(x1 - x2, 2.0D) + Math.pow(y1 - y2, 2.0D));
+    }
 }
--- Lab14/PrizePanel.java
+++ Lab14/PrizePanel.java
@@ -1,41 +1,76 @@
-   //Name:    Date:
-   import javax.swing.*;
-   import java.awt.*;
-   import java.awt.event.*;
-   import java.awt.image.*;
-    public class PrizePanel extends JPanel
-   {
-      private static final int FRAME = 400;
-      private static final Color BACKGROUND = new Color(204, 204, 204);
-      private BufferedImage myImage;
-       private Graphics myBuffer;
-      private Ball myBall;
-      private Polkadot myPDot;
-      private Timer myTimer; 
-		//constructor   
-       public PrizePanel()
-      {
-         
-      }
-       public void paintComponent(Graphics g)
-      {
-      
-      }
-       private class Listener implements ActionListener
-      {
-          public void actionPerformed(ActionEvent e)
-         {
-           
-         }
-      }   
-       private void collide(Ball ballIn, Polkadot pDot)
-      {
-        //double d = distance(  /* 4 arguments */  );  
-		
-		  
-      }
-       private double distance(double x1, double y1, double x2, double y2)
-      {
-         //return 	 // enter the calculation here.
-      }
-   }
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import java.net.URL;
+import javax.imageio.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+public class PrizePanel extends JPanel
+{
+    private static final int FRAME = 400;
+    private static final Color BACKGROUND = new Color(204, 204, 204);
+    private BufferedImage myImage;
+    private Graphics myBuffer;
+    private Ball myBall;
+    private Polkadot myPDot;
+    private Timer myTimer; 
+    private int hits = 0;
+    //constructor   
+    public PrizePanel()
+    {
+        myImage =  new BufferedImage(FRAME, FRAME, BufferedImage.TYPE_INT_RGB);
+        myBuffer = myImage.getGraphics();
+        myBuffer.setColor(BACKGROUND);
+        myBuffer.fillRect(0, 0, FRAME,FRAME);
+        int xPos = (int)(Math.random()*(FRAME-100) + 50);
+        int yPos = (int)(Math.random()*(FRAME-100)+ 50);
+        myBall = new Ball(xPos, yPos, 50, Color.BLACK);
+        xPos = (int)(Math.random()*(FRAME-100) + 50);
+        yPos = (int)(Math.random()*(FRAME-100)+ 50);
+        myPDot = new Polkadot(xPos,yPos,25,Color.RED); 
+
+        myTimer = new Timer(5, new Listener());
+        myTimer.start();
+
+    }
+
+    public void paintComponent(Graphics g)
+    {
+        g.drawImage (myImage, 0 , 0, FRAME, FRAME, null);
+    }
+    private class Listener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            myBuffer.setColor(BACKGROUND);
+            myBuffer.fillRect(0, 0, FRAME, FRAME);
+            myBall.move(FRAME, FRAME);
+            collide(myBall, myPDot);
+            myBall.draw(myBuffer);
+            myPDot.draw(myBuffer);
+            myBuffer.setColor(Color.BLACK);
+            myBuffer.setFont(new Font("Monospaced", Font.BOLD, 24));
+            myBuffer.drawString("Count: " + hits, FRAME - 150, 25);
+            repaint();
+            
+        }
+    }   
+    private void collide(Ball ballIn, Polkadot pDot)
+    {
+        double d = distance(ballIn.getX(), ballIn.getY(), pDot.getX(), pDot.getY() );  
+        if (d<= ballIn.getDiameter()/2 + pDot.getRadius())
+        {
+            hits++;
+        }
+    }
+
+    private double distance(double x1, double y1, double x2, double y2)
+    {
+        return  Math.sqrt(Math.pow(x1-x2 , 2) + Math.pow(y1-y2 , 2));  
+
+    }
+}
